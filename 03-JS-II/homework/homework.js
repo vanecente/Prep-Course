64786468345560336b6c77d151f2a3d045ee4f1d
vanecente <vanecente@gmail.com>
2022-07-10 12:39:38 -0300

    first commit

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -4,7 +4,7 @@ function obtenerMayor(x, y) {
   // "x" e "y" son números enteros (int).
   // Devuelve el número más grande
   // Si son iguales, devuelve cualquiera de los dos
-  // Tu código:
+   return ( e < y ); ""
 }
 
 function mayoriaDeEdad(edad) {
